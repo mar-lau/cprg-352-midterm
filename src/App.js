c8bc96b93e118379f13af963ef5ed40c5971f0c6
Marcus Lau <marcus.lau@edu.sait.ca>
2022-06-29 13:01:34 -0600

    Initial react application
    Create an react application with react-router-dom and three pages
    About, Contact, and Home.

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+import Home from './component/Home';
+import About from './component/About';
+import Contact from './component/Contact';
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+        </ul>
+        <Routes>
+          <Route exact path='/' element={< Home />}></Route>
+          <Route exact path='/about' element={< About />}></Route>
+          <Route exact path='/contact' element={< Contact />}></Route>
+        </Routes>
+        
+      </div>
+    </Router>
+  );
+}
+
+export default App;
